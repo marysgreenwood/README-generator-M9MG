aea2f75a1269370933583a3a75a3536f6fa70e44
marysgreenwood <marysgreenwood@gmail.com>
2023-01-18 10:55:39 -0500

    modularized inquirer prompts and wrote init function

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
-README generator
+# a
+![GitHub license](https://img.shields.io/badge/license-BSD 2-clause "Simplified" license-blue.svg)
 
-Created in fulfillment of UNC Coding Bootcamp Module 9 Challenge requirements.
+## Description
+n
+
+##Installation
+d
+
+##Usage
+s
+
+##Contribution guidelines
+v
+
+##Test instructions
+z
+
+##License
+## License
+
+This project is licensed under the BSD 2-clause "Simplified" license license.
+
+* [License](#license)
+
+
+##Questions
+Github: https://github.com/t
+Email: g
+
--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 // array of questions for user input
-//const questions = [];
-inquirer.prompt ([
+const questions = [
     {
         type: 'input',
         name: 'title',
@@ -70,60 +69,28 @@ inquirer.prompt ([
         message: 'Please enter your email address'
     }
 
-])
-.then((data)=> {
-    console.log (JSON.stringify(data));
-    fs.writeFile ('README.md', generateMarkdown(data))
-})
+]
+// .then((data)=> {
+//     console.log (JSON.stringify(data));
+//     fs.writeFile ('README.md', generateMarkdown(data))
+// })
 
 
 // function to write README file
-// function writeToFile(fileName, data) {
-//     fs.writeFile ('README.md')
-// }
+function writeToFile(fileName, data) {
+    fs.writeFile (fileName, data, (err) => {
+        err ? console.error(err) : console.log('Success!')
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt (questions). then ((data) => {
+        writeToFile('./output/README.md', generateMarkdown(data))
+    } )
+}
 
 // Function call to initialize app
 init();
 
-/* 
-Apache license 2.0	apache-2.0
 
-Artistic license 2.0	artistic-2.0
-Boost Software License 1.0	bsl-1.0
-
-BSD 2-clause "Simplified" license	bsd-2-clause
-BSD 3-clause "New" or "Revised" license	bsd-3-clause
-BSD 3-clause Clear license	bsd-3-clause-clear
-Creative Commons license family	cc
-Creative Commons Zero v1.0 Universal	cc0-1.0
-Creative Commons Attribution 4.0	cc-by-4.0
-Creative Commons Attribution Share Alike 4.0	cc-by-sa-4.0
-Educational Community License v2.0	ecl-2.0
-Eclipse Public License 1.0	epl-1.0
-Eclipse Public License 2.0	epl-2.0
-European Union Public License 1.1	eupl-1.1
-GNU Affero General Public License v3.0	agpl-3.0
-GNU General Public License family	gpl
-GNU General Public License v2.0	gpl-2.0
-
-GNU General Public License v3.0	gpl-3.0
-
-GNU Lesser General Public License family	lgpl
-GNU Lesser General Public License v2.1	lgpl-2.1
-GNU Lesser General Public License v3.0	lgpl-3.0
-ISC	isc
-LaTeX Project Public License v1.3c	lppl-1.3c
-Microsoft Public License	ms-pl
-
-MIT	mit
-
-Mozilla Public License 2.0	mpl-2.0
-Open Software License 3.0	osl-3.0
-PostgreSQL License	postgresql
-SIL Open Font License 1.1	ofl-1.1
-University of Illinois/NCSA Open Source License	ncsa
-The Unlicense	unlicense
-zLib License	zlib */
